56c6b059cabf161bbe13439f5e9c6219cbf5cbb1
reon <reonho@gmail.com>
2020-03-17 09:10:26 +0800

    Update firebase.js
    firebase api

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,14 @@ import firebase from 'firebase';
 
 const firebaseConfig = {
     //Key in your database details
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: ""
+    apiKey: "AIzaSyDHhEW02VrdAGmyF2Eo3-gsvTv1jBtWETk",
+    authDomain: "bt3103-75b69.firebaseapp.com",
+    databaseURL: "https://bt3103-75b69.firebaseio.com",
+    projectId: "bt3103-75b69",
+    storageBucket: "bt3103-75b69.appspot.com",
+    messagingSenderId: "297783790191",
+    appId: "1:297783790191:web:89751b965d4acf0c5adb9a",
+    measurementId: "G-ZTTZ765GRK"
   };
   
   firebase.initializeApp(firebaseConfig);
