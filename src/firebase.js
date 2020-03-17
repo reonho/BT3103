716fb6b974ed1e234026802ed30c3dd1afa190fd
reon <reonho@gmail.com>
2020-03-17 09:21:56 +0800

    Update firebase.js
    DELETE APIKEY

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import firebase from 'firebase';
 
 const firebaseConfig = {
     //Key in your database details
-    apiKey: "AIzaSyDHhEW02VrdAGmyF2Eo3-gsvTv1jBtWETk",
-    authDomain: "bt3103-75b69.firebaseapp.com",
-    databaseURL: "https://bt3103-75b69.firebaseio.com",
-    projectId: "bt3103-75b69",
-    storageBucket: "bt3103-75b69.appspot.com",
-    messagingSenderId: "297783790191",
-    appId: "1:297783790191:web:89751b965d4acf0c5adb9a",
-    measurementId: "G-ZTTZ765GRK"
+
   };
   
   firebase.initializeApp(firebaseConfig);
